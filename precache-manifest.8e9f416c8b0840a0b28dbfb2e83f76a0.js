74cbedf68e312dfcbc0a23a44e61e5695d59d519
Agprashant7 <prashantag420@gmail.com>
2020-04-24 16:53:55 +0530

    first commit

---
+++ precache-manifest.8e9f416c8b0840a0b28dbfb2e83f76a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18f5d12ee36d5dc8e3efe49a3c19f09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa008ebf6a3c66d288e2",
+    "url": "/static/css/main.483e7f34.chunk.css"
+  },
+  {
+    "revision": "02415294cce5185440f3",
+    "url": "/static/js/2.47fdfe42.chunk.js"
+  },
+  {
+    "revision": "86cf7f074fde330b07dbd8e5fbabf90d",
+    "url": "/static/js/2.47fdfe42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aa008ebf6a3c66d288e2",
+    "url": "/static/js/main.b5d706c4.chunk.js"
+  },
+  {
+    "revision": "6dc4bff3df7395cb915f",
+    "url": "/static/js/runtime-main.48984e02.js"
+  }
+]);
